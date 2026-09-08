ff2d477f86ed82bb55ad1d3294e31eb7e49fdce9
Ryusei takai <bbryusei7515@gmail.com>
2026-09-08 11:03:46 +0900

    実装手順 7.カート一覧・合計金額 の実装

--- CLI-EC-Shop/src/service/CartService.java
+++ CLI-EC-Shop/src/service/CartService.java
@@ -12,6 +12,7 @@ public class CartService {
 		return cart;
 	}
 
+	// カート追加処理	
 	public boolean addToCart(Product product, int quantity) {
 
 		//在庫数確認		
@@ -25,6 +26,41 @@ public class CartService {
 		cart.getItems().add(newCartItem);
 
 		return true;
+	}
+
+	//カート内合計金額計算処理
+	public int calculateTotalPrice() {
+		int totalPrice = 0;
+
+		// cart.getItems()をfor文で繰り返す
+		for (CartItem currentCartItem : cart.getItems()) {
+			totalPrice += currentCartItem.getProduct().getPrice() * currentCartItem.getQuantity();
+		}
+
+		return totalPrice;
+	}
+
+	public void showCartList() {
+		System.out.println("====================");
+		System.out.println("　　　カート内一覧　　　");
+		System.out.println("====================");
+		System.out.printf("%-4s %-18s %-12s %-8s %-8s", "ID", "商品名", "価格", "数量", "小計");
+		System.out.println();
+
+		for (CartItem currentCartItem : cart.getItems()) {
+			int subtotal = currentCartItem.getProduct().getPrice() * currentCartItem.getQuantity();
+
+			System.out.printf("%-4s %-18s %-12s %-8s %-8s%n",
+					currentCartItem.getProduct().getProductId(),
+					currentCartItem.getProduct().getProductName(),
+					currentCartItem.getProduct().getPrice(),
+					currentCartItem.getQuantity(),
+					subtotal);
+		}
+
+		System.out.println("--------------------");
+		System.out.println("合計金額：" + calculateTotalPrice() + "円");
+		System.out.println();
 	}
 
 }
